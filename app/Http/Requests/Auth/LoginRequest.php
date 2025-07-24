b6a653ec1f7494daea414c06badc69b83c3bf7de
navod <navodthishan@gmail.com>
2025-07-24 09:03:11 +0530

    Paginate apply

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -15,17 +15,7 @@ class LoginRequest extends FormRequest
     {
         return [
             'email' => ['required', 'string', 'email', 'max:255'],
-            'password' => ['required', 'string', 'min:6'],
-        ];
-    }
-
-    public function messages(): array
-    {
-        return [
-            'email.required' => 'Email is required',
-            'email.email' => 'Please provide a valid email address',
-            'password.required' => 'Password is required',
-            'password.min' => 'Password must be at least 6 characters',
+            'password' => ['required', 'string'],
         ];
     }
 }
